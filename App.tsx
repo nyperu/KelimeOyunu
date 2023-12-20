ca7d650914ce6aa398c13ae8d8c832466c6d802b
Oğuzhan Koca <0ogzhn000@gmail.com>
2023-12-20 23:24:17 +0300

    oyunun genel yapılanması

--- App.tsx
+++ App.tsx
@@ -1,118 +1,270 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, StyleSheet, ScrollView } from 'react-native';
+import FlipCardComponent from './FlipCardComponent'; // FlipCardComponent'in import edildiği yer
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+// Meyve objesi için bir tip tanımlaması
+interface Fruit {
+  id: string;
+  en: string;
+  tr: string;
 }
+// Tüm meyvelerin listesi
+const allFruits: Fruit[] =[
+  // ... 30 meyve içeren allFruits dizisi
+  
+    { id: 'apple', en: 'Apple', tr: 'Elma' },
+    { id: 'banana', en: 'Banana', tr: 'Muz' },
+    { id: 'orange', en: 'Orange', tr: 'Portakal' },
+    { id: 'strawberry', en: 'Strawberry', tr: 'Çilek' },
+    { id: 'grape', en: 'Grape', tr: 'Üzüm' },
+    { id: 'cherry', en: 'Cherry', tr: 'Kiraz' },
+    { id: 'watermelon', en: 'Watermelon', tr: 'Karpuz' },
+    { id: 'peach', en: 'Peach', tr: 'Şeftali' },
+    { id: 'pear', en: 'Pear', tr: 'Armut' },
+    { id: 'pineapple', en: 'Pineapple', tr: 'Ananas' },
+    { id: 'mango', en: 'Mango', tr: 'Mango' },
+    { id: 'blueberry', en: 'Blueberry', tr: 'Yaban Mersini' },
+    { id: 'kiwi', en: 'Kiwi', tr: 'Kivi' },
+    { id: 'lemon', en: 'Lemon', tr: 'Limon' },
+    { id: 'lime', en: 'Lime', tr: 'Lime' },
+    { id: 'papaya', en: 'Papaya', tr: 'Papaya' },
+    { id: 'raspberry', en: 'Raspberry', tr: 'Ahududu' },
+    { id: 'blackberry', en: 'Blackberry', tr: 'Böğürtlen' },
+    { id: 'coconut', en: 'Coconut', tr: 'Hindistan Cevizi' },
+    { id: 'fig', en: 'Fig', tr: 'İncir' },
+    { id: 'guava', en: 'Guava', tr: 'Guava' },
+    { id: 'lychee', en: 'Lychee', tr: 'Liçi' },
+    { id: 'nectarine', en: 'Nectarine', tr: 'Nektarin' },
+    { id: 'olive', en: 'Olive', tr: 'Zeytin' },
+    { id: 'pomegranate', en: 'Pomegranate', tr: 'Nar' },
+    { id: 'apricot', en: 'Apricot', tr: 'Kayısı' },
+    { id: 'avocado', en: 'Avocado', tr: 'Avokado' },
+    { id: 'blackcurrant', en: 'Blackcurrant', tr: 'Kara Frenk Üzümü' },
+    { id: 'cantaloupe', en: 'Cantaloupe', tr: 'Kavun' },
+    { id: 'grapefruit', en: 'Grapefruit', tr: 'Greyfurt' }
+  ];
+  
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const App = () => {
+  // fruits state'i, ekranda gösterilen meyveleri tutar
+  // Başlangıçta 12 rastgele meyve seçmek için bir döngü kullanın
+  const getRandomFruits = () => {
+    const randomFruits = [];
+    for (let i = 0; i < 12; i++) {
+      const randomFruit = allFruits[Math.floor(Math.random() * allFruits.length)];
+      randomFruits.push({ ...randomFruit, id: randomFruit.id + Date.now() + i }); // Benzersiz bir id ekleyin
+    }
+    return randomFruits;
+  };
+
+  const [fruits, setFruits] = useState(getRandomFruits());
+
+  // Bir kart flip edildiğinde çağrılan fonksiyon
+  
+  const handleCardFlipEnd = (id:string) => {
+    setFruits(currentFruits => {
+      // Flip edilen kartın indeksini bul
+      const index = currentFruits.findIndex(fruit => fruit.id === id);
+
+      // Yeni meyve ekleyin
+      const newFruit = allFruits[Math.floor(Math.random() * allFruits.length)];
+      const newFruitWithUniqueID = { ...newFruit, id: newFruit.id + Date.now() };
+
+      // Yeni meyveyi eski meyvenin yerine ekleyin
+      const newFruits = [...currentFruits];
+      newFruits.splice(index, 1, newFruitWithUniqueID);
+
+      return newFruits;
+    });
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+    <SafeAreaView style={styles.container}>
+      {/* Üst bölüm: Başlık */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>KELİME OYUNU</Text>
+      </View>
+
+      {/* Orta bölüm: Kartlar */}
+      <ScrollView contentContainerStyle={styles.cardContainer}>
+        {fruits.map((fruit, index) => (
+          <FlipCardComponent
+            id={fruit.id}
+            key={fruit.id}
+            frontText={fruit.en}
+            backText={fruit.tr}
+            onFlipEnd={() => handleCardFlipEnd(fruit.id)}
+          />
+        ))}
       </ScrollView>
+
+      {/* Alt bölüm: Footer */}
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>By Oğuzhan Koca</Text>
+      </View>
     </SafeAreaView>
   );
-}
+};
 
+// Stil tanımlamaları
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
   },
-  sectionDescription: {
-    marginTop: 8,
+  headerText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  footer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+  },
+  footerText: {
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    color: '#333',
   },
 });
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { SafeAreaView, View, Text, StyleSheet, ScrollView } from 'react-native';
+// import FlipCardComponent from './FlipCardComponent';
+
+// const allFruits = [
+//   { id: 'apple', en: 'Apple', tr: 'Elma' },
+//   { id: 'banana', en: 'Banana', tr: 'Muz' },
+//   { id: 'orange', en: 'Orange', tr: 'Portakal' },
+//   { id: 'strawberry', en: 'Strawberry', tr: 'Çilek' },
+//   { id: 'grape', en: 'Grape', tr: 'Üzüm' },
+//   { id: 'cherry', en: 'Cherry', tr: 'Kiraz' },
+//   { id: 'watermelon', en: 'Watermelon', tr: 'Karpuz' },
+//   { id: 'peach', en: 'Peach', tr: 'Şeftali' },
+//   { id: 'pear', en: 'Pear', tr: 'Armut' },
+//   { id: 'pineapple', en: 'Pineapple', tr: 'Ananas' },
+//   { id: 'mango', en: 'Mango', tr: 'Mango' },
+//   { id: 'blueberry', en: 'Blueberry', tr: 'Yaban Mersini' },
+//   { id: 'kiwi', en: 'Kiwi', tr: 'Kivi' },
+//   { id: 'lemon', en: 'Lemon', tr: 'Limon' },
+//   { id: 'lime', en: 'Lime', tr: 'Lime' },
+//   { id: 'papaya', en: 'Papaya', tr: 'Papaya' },
+//   { id: 'raspberry', en: 'Raspberry', tr: 'Ahududu' },
+//   { id: 'blackberry', en: 'Blackberry', tr: 'Böğürtlen' },
+//   { id: 'coconut', en: 'Coconut', tr: 'Hindistan Cevizi' },
+//   { id: 'fig', en: 'Fig', tr: 'İncir' },
+//   { id: 'guava', en: 'Guava', tr: 'Guava' },
+//   { id: 'lychee', en: 'Lychee', tr: 'Liçi' },
+//   { id: 'nectarine', en: 'Nectarine', tr: 'Nektarin' },
+//   { id: 'olive', en: 'Olive', tr: 'Zeytin' },
+//   { id: 'pomegranate', en: 'Pomegranate', tr: 'Nar' },
+//   { id: 'apricot', en: 'Apricot', tr: 'Kayısı' },
+//   { id: 'avocado', en: 'Avocado', tr: 'Avokado' },
+//   { id: 'blackcurrant', en: 'Blackcurrant', tr: 'Kara Frenk Üzümü' },
+//   { id: 'cantaloupe', en: 'Cantaloupe', tr: 'Kavun' },
+//   { id: 'grapefruit', en: 'Grapefruit', tr: 'Greyfurt' }
+// ];
+
+// const App = () => {
+//   const [fruits, setFruits] = useState([
+//     { en: 'Apple', tr: 'Elma' },
+//     { en: 'Banana', tr: 'Muz' },
+//     { en: 'Orange', tr: 'Portakal' },
+//     { en: 'Apple', tr: 'Elma' },
+//     { en: 'Banana', tr: 'Muz' },
+//     { en: 'Orange', tr: 'Portakal' },
+//     { en: 'Apple', tr: 'Elma' },
+//     { en: 'Banana', tr: 'Muz' },
+//     { en: 'Orange', tr: 'Portakal' },
+//     { en: 'Apple', tr: 'Elma' },
+//     { en: 'Banana', tr: 'Muz' },
+//     { en: 'Orange', tr: 'Portakal' },
+    
+//   ]);
+
+//   const handleCardFlipEnd = (id) => {
+//     setFruits(currentFruits => currentFruits.filter(fruit => fruit.id !== id));
+//   };
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <View style={styles.header}>
+//         <Text style={styles.headerText}>KELİME OYUNU</Text>
+//       </View>
+
+//       <ScrollView contentContainerStyle={styles.cardContainer}>
+//       {fruits.map((fruit) => (
+//         <FlipCardComponent
+//           key={fruit.id}
+//           frontText={fruit.en}
+//           backText={fruit.tr}
+//           onFlipEnd={() => handleCardFlipEnd(fruit.id)}
+//         />
+//       ))}
+//     </ScrollView>
+
+//       <View style={styles.footer}>
+//         <Text style={styles.footerText}>By Oğuzhan Koca</Text>
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   header: {
+//      flex: 2,
+    
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#f8f8f8',
+//   },
+//   headerText: {
+//     fontSize: 22,
+//     fontWeight: 'bold',
+//   },
+//   cardContainer: {
+    
+//     flexDirection: 'row',
+//     flexWrap: 'wrap',
+//     justifyContent: 'space-around',
+//     alignItems: 'center',
+//   },
+//   footer: {
+//      flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#f8f8f8',
+//   },
+//   footerText: {
+//     fontSize: 18,
+//     color: '#333',
+//   },
+// });
+
+// export default App;
